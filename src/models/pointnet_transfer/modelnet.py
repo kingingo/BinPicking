508ad3951c9b6d21eeb5af9e158b1b214c0786f4
Felix <administrator>
2023-02-16 18:02:23 +0100

    test

--- src/models/pointnet_transfer/modelnet.py
+++ src/models/pointnet_transfer/modelnet.py
@@ -21,6 +21,7 @@ def _create_input_pairs(dataset_path, training):
             continue;
             
         
+        print(f"folder found {subfolder}")
         files_in_subfolder = os.listdir(subfolder)
         data_pairs.extend([(osp.join(subfolder, fn), idx) for fn in files_in_subfolder])
     return data_pairs, label_strings
